ace59895b357cf0b0a18fc6652ce5bbb05fea289
thcvaz22 <thcvaz22@gmail.com>
2026-08-13 01:21:26 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// Rodízio RGdB - Service Worker v10
+// Rodízio RGdB - Service Worker v12
 // Objetivo: sempre buscar a versão mais nova quando houver rede e manter fallback offline confiável.
 const CACHE_PREFIX = 'rodizio-rgdb-';
-const CACHE_NAME = CACHE_PREFIX + 'shell-20260813-v10';
+const CACHE_NAME = CACHE_PREFIX + 'shell-20260813-v12';
 const APP_SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
